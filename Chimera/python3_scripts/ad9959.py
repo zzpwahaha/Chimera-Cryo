12e80157c25a3fddbd0306f93f1dc881c9be5a67
Zhenpu Zhang <Zhenpu.Zhang@colorado.edu>
2021-07-19 18:33:11 -0600

    same problem as previous

--- Chimera/python3_scripts/ad9959.py
+++ Chimera/python3_scripts/ad9959.py
@@ -8,17 +8,18 @@ def writeToDDS(dds_char_dev, address, data):
   # print(struct.unpack('<I', data_bytes))
   words = []
   #instruction and D16-D31, D31 is MSB zzp
-  words.append(data_bytes[2])
-  words.append(data_bytes[3])
+  words.append(data_bytes[2:3])
+  words.append(data_bytes[3:4])
   words.append(struct.pack('<B', address + 128))
   words.append(b'\x00')
   #IOU and D0-D15
-  words.append(data_bytes[0])
-  words.append(data_bytes[1])
+  words.append(data_bytes[0:1])
+  words.append(data_bytes[1:2])
   words.append(struct.pack('<B', 192 + 1))
   words.append(b'\x00')
-  print('0x'+binascii.hexlify(''.join(words[3:None:-1])), \
-        ' 0x'+binascii.hexlify(''.join(words[-1:3:-1])))
+  print(words)
+  print('0x'+''.join([by.hex() for by in words[3:None:-1]]), \
+        ' 0x'+''.join([by.hex() for by in words[-1:3:-1]]))
   for word in words:
     dds_char_dev.write(word)
 
@@ -36,7 +37,7 @@ class AD9959:
 
   def set_DDS(self, channel, freq, amp = None, DACScale = None):
     assert channel>=0 and channel<=3, 'Invalid channel for AD9959 in set_DDS'
-    with open(self.device, "r+b") as character:
+    with open(self.device, "wb") as character:
       # ~ writeWords(character)
       freqConv = int((2**32 - 1)*freq/500)
       if amp is None:
--- Chimera/python3_scripts/dds_lock_pll.py
+++ Chimera/python3_scripts/dds_lock_pll.py
@@ -7,18 +7,19 @@ def writeToDDS(dds_char_dev, address, data):
   # print(struct.unpack('<I', data_bytes))
   words = []
   #instruction and D16-D31
-  words.append(data_bytes[2])
-  words.append(data_bytes[3])
+  words.append(data_bytes[2:3]) # in python2 data_bytes[2] returns the third bytes in b'\x..' format, but in python3 data_bytes[2] returns the integer of that byte, need to use data_bytes[2:3]
+  words.append(data_bytes[3:4])
   words.append(struct.pack('<B', address + 128))
-  words.append("\x00")
+  words.append(b"\x00")
   #IOU and D0-D15
-  words.append(data_bytes[0])
-  words.append(data_bytes[1])
+  words.append(data_bytes[0:1])
+  words.append(data_bytes[1:2])
   words.append(struct.pack('<B', 192 + 1))
-  words.append("\x00")
-
+  words.append(b"\x00")
+  print(words)
   for word in words:
     dds_char_dev.write(word)
+  dds_char_dev.flush()
 
 def dds_lock_pll():
   print("writing to 5000")
