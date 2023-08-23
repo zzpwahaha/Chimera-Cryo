be10bf87b86d08fa15d1c28710de979d0189a5e6
Zhenpu Zhang <Zhenpu.Zhang@colorado.edu>
2023-08-23 14:30:09 -0600

    CORE UPDATE of analysisThread to revive the realtime analysis

--- Chimera/Source/RealTimeDataAnalysis/AnalysisThreadWorker.h
+++ Chimera/Source/RealTimeDataAnalysis/AnalysisThreadWorker.h
@@ -25,32 +25,32 @@ class AnalysisThreadWorker : public QObject {
     private:
 		// subroutine for handling atom & count plots
 		std::vector<std::vector<dataPoint>> handlePlotAtoms (
-			PlottingInfo plotInfo, unsigned repNum, std::vector<std::vector<std::pair<double, unsigned long>> >& finData,
+			PlottingInfo plotInfo, picureStatus picStat, std::vector<std::vector<std::vector<std::pair<double, unsigned long>>>>& finData,
 			std::vector<std::vector<dataPoint>>& dataContainers,
-			unsigned variationNumber, std::vector<std::vector<bool>>& pscSatisfied,
-			int plotNumberCount, std::vector<std::vector<int> > atomPresent, unsigned groupNum);
+			std::vector<std::vector<bool>>& pscSatisfied,
+			std::vector<std::vector<int> > atomPresent, unsigned groupNum);
 		std::vector<std::vector<dataPoint>> handlePlotHist (
-			PlottingInfo plotInfo, std::vector<std::vector<long>> countData,
-			std::vector<std::vector<std::deque<double>>>& finData, std::vector<std::vector<bool>>pscSatisfied,
+			PlottingInfo plotInfo, std::vector<std::vector<double>> countData,
+			std::vector<std::vector<bool>>pscSatisfied,
 			std::vector<std::vector<std::map<int, std::pair<int, unsigned long>>>>& histData,
-			std::vector<std::vector<dataPoint>>& dataContainers, unsigned groupNum, int pltNumberCount);
+			std::vector<std::vector<dataPoint>>& dataContainers, unsigned groupNum);
 		void determineWhichPscsSatisfied (
 			PlottingInfo& info, unsigned groupSize, std::vector<std::vector<int>> atomPresentData, 
 			std::vector<std::vector<bool>>& pscSatisfied);
 
         std::vector<double> xvals;
         realTimePlotterInput* input;
-        std::vector < std::vector<std::vector<dataPoint>>> dataContainers;
-        std::vector<std::vector<std::vector<long>>> countData;
-        std::vector<std::vector<std::vector<int>>> atomPresentData;
-        std::vector<std::vector<std::vector<std::vector<long> > > > finalCountData;
-        std::vector<std::vector<std::vector<std::pair<double, unsigned long> > > > finalDataNew;
-        std::vector<std::vector<std::vector<std::vector<double>>>> finalAvgs;
+        std::vector<std::vector<std::vector<dataPoint>>> dataContainers; // [plot][atom+avg][var] -> dataPoint
+        std::vector<std::vector<std::vector<double>>> countData; // [grid][atom][pic]
+        std::vector<std::vector<std::vector<int>>> atomPresentData; // [grid][atom][pic]
+        //std::vector<std::vector<std::vector<std::vector<long> > > > finalCountData;
+        std::vector<std::vector<std::vector<std::vector<std::pair<double, unsigned long>>>>> finalAtomData; // [plot][dataset][atom][var] -> <num of positive event, total event after post selection>
+        //std::vector<std::vector<std::vector<std::vector<double>>>> finalAvgs;
         // Averaged over all pixels (avgAvg is the average of averages over repetitions)
         //std::vector<std::vector<std::vector<double>>> avgAvg;
         //std::vector<std::vector<std::vector<bool> > > newData;
-        std::vector<std::vector<std::vector<std::deque<double>>>> finalHistData, finalErrorBars, finalXVals;
-        std::vector<std::vector<std::vector<std::map<int, std::pair<int, unsigned long>>>>> histogramData;
+        //std::vector<std::vector<std::vector<std::deque<double>>>> finalHistData, finalErrorBars, finalXVals;
+        std::vector<std::vector<std::vector<std::map<int, std::pair<int, unsigned long>,std::less<int>>>>> histogramData; // [plot][dataset][atom], this is sorted in ascending order, explicit specific std::less even though it is by default
         unsigned noAtomsCounter = 0, atomCounterTotal = 0, picNumberCount = 0;
         std::vector<PlottingInfo> allPlots;
 };
