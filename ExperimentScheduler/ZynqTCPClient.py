4f26890ca83eeec9d7dc03f652a8087d04ffd384
Zhenpu Zhang <Zhenpu.Zhang@colorado.edu>
2024-08-13 16:28:33 -0600

    small wrapper for connecting to zynq and let it quit

---
+++ ExperimentScheduler/ZynqTCPClient.py
@@ -0,0 +1,26 @@
+import socket
+import sys
+from time import sleep
+from TCPClient import TCPClient
+
+
+class ZynqTCPClient(TCPClient):
+    def __init__(self, host, port, timeout=10):
+        super().__init__(host, port, timeout)
+        self.connectByteLen = 64
+        self.dioByteLen = 28
+        self.dacByteLen = 42
+        self.ddsByteLen = 46
+        super().connect()
+
+    def _sendMessage(self, message : bytes, length):
+        messagePad = message.ljust(length, b'\0')
+        super().send_message(messagePad)
+
+
+if __name__ == "__main__":
+    HOST = '10.10.0.2'
+    PORT = 8080
+    client = ZynqTCPClient(HOST, PORT)
+    client.send_message("QUIT");
+    # sleep(1)
