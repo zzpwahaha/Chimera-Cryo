cdba82e5954e145202e99e758001a0051de0b76d
Zhenpu Zhang <Zhenpu.Zhang@colorado.edu>
2025-06-09 21:50:06 -0600

    do NOT pop the queue in gigamoog so that it can work for variation first

--- Chimera/Source/GigaMOOG/MessageSender.cpp
+++ Chimera/Source/GigaMOOG/MessageSender.cpp
@@ -30,7 +30,7 @@ std::vector<int> MessageSender::getMessageBytes()
 			bytevector.push_back(byte);
 		}
 	}
-	queue.clear();
+	//queue.clear(); ZZP: commented out 20250607. This shouldn't clear the queue otherwise can not call this function again. Not sure what is their initial idea
 	return bytevector;
 }
 
